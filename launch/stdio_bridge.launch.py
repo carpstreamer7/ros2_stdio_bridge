b12449df23997947e88ce521d961114a3d0819f9
Morofuji Junko <mimosa4kai@gmail.com>
2025-12-26 20:01:18 +0900

    stdio_bridge.launch.py追加、それに伴うsetup.pyの編集

---
+++ launch/stdio_bridge.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ros2_stdio_bridge',
+            executable='stdin_bridge',
+            name='stdin_bridge'
+        ),
+        Node(
+            package='ros2_stdio_bridge',
+            executable='stdout_sink',
+            name='stdout_sink'
+        )
+    ])
+
